60021594c500ecbf19b71fecda7da4f766d168d4
tcandra24 <tcandra007@gmail.com>
2024-07-18 14:20:31 +0700

    Use Route

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import router from "./routes";
 
 import "./style.css";
 import App from "./App.vue";
@@ -7,5 +8,6 @@ import App from "./App.vue";
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(router);
 app.use(pinia);
 app.mount("#app");
